a0e9b976b8ffcf9c481c716defd7592ba4949236
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-10-03 13:50:55 +0545

    firebase complete package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:ecom_ui/auth_screen/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
